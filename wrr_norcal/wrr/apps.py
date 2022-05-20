bfb08e4a6504a1eada44db043b6364abfd4a0840
Hugo <hugotinoco@icloud.com>
2022-05-20 07:01:33 +0000

    black

--- wrr_norcal/wrr/apps.py
+++ wrr_norcal/wrr/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WrrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wrr'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wrr"
